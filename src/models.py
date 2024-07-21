15f6b53ca6ce047644422639742b6e5043b02255
Bárbara Ferreira <166528369+TheTiredTiger@users.noreply.github.com>
2024-07-21 23:20:06 +0000

    First test

--- src/models.py
+++ src/models.py
@@ -7,12 +7,14 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+class User(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    fave_character = Column(String(250), nullable=True)
+    fave_planet = Column(String(250), nullable=True)
 
 class Address(Base):
     __tablename__ = 'address'
@@ -23,7 +25,7 @@ class Address(Base):
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    person = relationship(User)
 
     def to_dict(self):
         return {}
