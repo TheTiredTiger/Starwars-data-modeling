0bf3fa863a87f30ed58ef2ab005de786818cbebc
Bárbara Ferreira <166528369+TheTiredTiger@users.noreply.github.com>
2024-07-22 11:56:22 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -8,27 +8,37 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'person'
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    fave_character = Column(String(250), nullable=True)
-    fave_planet = Column(String(250), nullable=True)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favorites = Column(Integer, ForeignKey('favorites.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Characters(Base):
+    __tablename__ = 'characters'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(User)
+    name = Column(String(250), nullable=False)
+    
+class Plantets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    favorite_planets = Column(Integer, ForeignKey('planets.id'))
+    favorite_characters = Column(Integer, ForeignKey('characters.id'))
 
     def to_dict(self):
         return {}
-
+    
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
